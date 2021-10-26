ecedc9a3b325bd45e2c8c63aa02134c2fa42e03e
LenyPython <buniadyserv@gmail.com>
2021-10-26 16:40:35 +0200

    Created custom panel depending on an account admin status

--- src/components/AccountPanel/AccountPanel.tsx
+++ src/components/AccountPanel/AccountPanel.tsx
@@ -1,43 +1,14 @@
 import AdminPanel from './AdminPanel/AdminPanel'
 import UserPanel from './UserPanel/UserPanel'
-import styled from 'styled-components'
-import { 
-  Switch,
-  Route,
-  Link,
-  useRouteMatch
-} from 'react-router-dom'
+import { useAppSelector } from '../../appStore/hooks'
+import { getUser } from '../../slices/user/user'
 
-const AccBtn = styled(Link)`
-  display: inline-block;
-  text-decoration: none;
-  text-align: center;
-  font-weight: 600;
-  margin: .5em 3em;
-  background: var(--main-color);
-  color: var(--main-button-font-color);
-  border-radius: 5px;
-  min-width: 80px;
-  padding: 1em;
-`
 
 const AccountPanel:React.FC = () =>{
-  let match = useRouteMatch()
-  return (<>
-    <div>
-    <AccBtn to={`${match.url}/admin`}>Admin</AccBtn>
-    <AccBtn to={`${match.url}/user`}>User</AccBtn>
-    </div>
-<Switch>
-        <Route path={`${match.path}/admin`}>
-        <AdminPanel />
-        </Route>
-        <Route path={`${match.path}/user`}>
-        <UserPanel />
-        </Route>
-</Switch>
-    </>
-  )
+  const { admin } = useAppSelector(getUser)
+  
+  if(admin) return <AdminPanel />
+  return <UserPanel />
 }
 
 export default AccountPanel
